cb53e1bdfb57dee73568a42df12abb1936a08bc1
lucas1284 <ferreirabjlucas@gmail.com>
2024-09-05 20:23:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,160 @@
+export const perguntas = [
+    {
+        enunciado: "Ano que o futebol surgiu no brasil?",
+        alternativas: [
+            {
+                texto: "1894",
+                afirmacao: [
+                    "Acertou."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "1990",
+                afirmacao: [
+                    "Errou."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: " Quem trouxe o futebol pro Brasil?",
+        alternativas: [
+            {
+                texto: "Charles Miler.",
+                afirmacao: [
+                    "Acertou."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Willian Miler.",
+                afirmacao: [
+                    "Errou."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qualfoi o primeiro estado do Brasil que o futebol chegou?",
+        alternativas: [
+            {
+                texto: "São Paulo.",
+                afirmacao: [
+                    "Conseguiu utilizar a IA para buscar informações úteis.",
+                    "Percebeu que a IA pode ajudar a encontrar informações úteis na internet de forma mais rápida e direcionada.",
+                    "Percebeu que a IA consegue explicar termos complicados de forma simplificada e isso ajudou muito suas pesquisas sobre assuntos complexos."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Rio de Janeiro.",
+                afirmacao: [
+                    "Sentiu mais facilidade em utilizar seus próprios recursos para escrever seu trabalho.",
+                    "Achou que era muito mais fácil procurar por respostas utilizando alguns meios mais tradicionais mesmo que levasse mais tempo.",
+                    "Sentiu um pouco de medo de quais dados pessoais seus a IA poderia utilizar e por isso prefere fazer suas coisas com pouca intromissão da tecnologia."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Qual primeiro time de futebol que srugiu no Brasil ?",
+        alternativas: [
+            {
+                texto: "São Paulo Athletic Club.",
+                afirmacao: [
+                    "Acertou."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Flamengo Athletic.",
+                afirmacao: [
+                    "Errou."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Utilizar informações próprias traz um toque pessoal ao nosso trabalho. Agora imagine que você precisa participar de um debate sobre o quanto a IA impacta no trabalho do futuro. Qual posicionamento tomar?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Primeiro Campeonato que teve no Brasil",
+        alternativas: [
+            {
+                texto: "Campeonato Paulista.",
+                afirmacao: [
+                    "Acertou."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Campeonato Brasileirão.",
+                afirmacao: [
+                    "Errou."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "É muito importante refletir sobre os trabalhadores do futuro. Por isso a professora realizou um debate entre a turma para entender como foi realizada a pesquisa e escrita. Nessa conversa também foi levantado um ponto muito importante: como a IA impacta o trabalho do futuro. Nesse debate, como você se posiciona?",
+        alternativas: [
+            {
+                texto: "Defende a ideia de que a IA pode criar novas oportunidades de emprego e melhorar habilidades humanas.",
+                afirmacao: [
+                    "Vem impulsionando a inovação na área de IA e luta para abrir novos caminhos profissionais com IA.",
+                    "Participa ativamente do desenvolvimento de soluções criativas e na melhoria de processos em IA."
+                ],
+                proxima: 7,
+            },
+            {
+                texto: "Me preocupo com as pessoas que perderão seus empregos para máquinas e defendem a importância de proteger os trabalhadores.",
+                afirmacao: [
+                    "Sua preocupação com as pessoas motivou a criar um grupo de estudos entre trabalhadores para discutir meios de utilização de IA de forma ética.",
+                    "Criou grupos de ética voltado para IA e busca ativamente reduzir as desigualdades geradas pela automação."
+                ],
+                proxima: 7,
+            },
+        ]
+    },
+    {
+        enunciado: "Maior time de hoje em dia no Brasil?",
+        alternativas: [
+            {
+                texto: "São Paulo",
+                afirmacao: [
+                    "Acertou."
+                ],
+            },
+            {
+                texto: "Flamengo.",
+                afirmacao: [
+                    "Errou."
+                ],
+            },
+        ]
+    }
+];
